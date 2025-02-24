b40ae95bf96736a1eb52e95a8534c1444352d5a6
sagar yenkure <sagaryenkure@vionsys.com>
2025-02-24 19:02:55 +0530

    feat: Add VoucherForm component and integrate modal in AdminLayout for voucher management

--- src/components/admin/AdminLayout.tsx
+++ src/components/admin/AdminLayout.tsx
@@ -11,14 +11,18 @@ import { Modal } from "antd";
 import AdminNotification from "./AdminNotification";
 import { PlusCircle } from "lucide-react";
 import { IconNotification } from "@tabler/icons-react";
+import VoucherForm from "./VoucherForm";
 
 const AdminLayout = () => {
   const [ModalOpen, setModalOpen] = useState(false);
   const [NotificationModalOpen, setNotificationModalOpen] = useState(false);
+  const [VoucherModal, setVoucherModal] = useState(false);
+
   const { data, isLoading: productIsLoading } = useQueryFunction(
     ["query"],
     getProductApi
   );
+
   const productList: IProduct[] = data?.productList || [];
 
   return (
@@ -37,6 +41,13 @@ const AdminLayout = () => {
         >
           <IconNotification />
           Notification
+        </button>
+        <button
+          onClick={() => setNotificationModalOpen(!NotificationModalOpen)}
+          className="w-full sm:w-auto shadow-[0_0_0_3px_#000000_inset] m-2 flex gap-2 items-center px-2 py-2 bg-transparent border border-white text-white rounded-lg font-bold transform hover:-translate-y-1 transition duration-400"
+        >
+          <IconNotification />
+          Vouchers
         </button>
       </div>
 
@@ -68,6 +79,16 @@ const AdminLayout = () => {
         onCancel={() => setNotificationModalOpen(!NotificationModalOpen)}
       >
         <AdminNotification />
+      </Modal>
+
+      {/* add Voucher modal */}
+      <Modal
+        width={600}
+        footer={null}
+        open={VoucherModal}
+        onCancel={() => setVoucherModal(!VoucherModal)}
+      >
+        <VoucherForm />
       </Modal>
     </>
   );
---
+++ src/components/admin/VoucherForm.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const VoucherForm = () => {
+  return <div>this features is in progress</div>;
+};
+
+export default VoucherForm;
