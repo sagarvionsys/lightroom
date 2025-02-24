4fa365ed269c822a7b122ef84efe6a6460cb8ed4
sagar yenkure <sagaryenkure@vionsys.com>
2025-02-24 12:13:12 +0530

    feat: Add notification fetching to Navbar and display unread count indicator

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import React from "react";
 import { FloatingDock } from "@/components/ui/floating-dock";
 import {
@@ -10,10 +11,21 @@ import {
   ShoppingCart,
 } from "lucide-react";
 import { signOut, useSession } from "next-auth/react";
+import { useQueryFunction } from "@/features/useQuery";
+import { getNotifications } from "@/services/NotificationsApi";
+import { INotification } from "@/types/notification.types";
 
 export function Navbar() {
   const { data: session } = useSession();
   const isLogin = session?.user;
+
+  // Fetch notifications
+  const { data: notifications } = useQueryFunction(
+    ["notifications"],
+    getNotifications
+  );
+
+  const hasUnread = notifications?.some((note: INotification) => !note.isRead);
 
   const links = [
     {
@@ -25,11 +37,18 @@ export function Navbar() {
       ? [
           {
             title: "Notifications",
-            icon: <BellDot size={28} absoluteStrokeWidth />,
+            icon: (
+              <div className="relative">
+                <BellDot size={28} absoluteStrokeWidth />
+                {hasUnread && (
+                  <span className="absolute top-1 right-0.5 w-3 h-3 bg-red-500 rounded-full"></span>
+                )}
+              </div>
+            ),
             href: "/notifications",
           },
           {
-            title: "cart",
+            title: "Cart",
             icon: <ShoppingCart size={28} absoluteStrokeWidth />,
             href: "/cart",
           },
