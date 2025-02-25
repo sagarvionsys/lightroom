9398a52b8512217aa20b8e710870104df1ab5546
sagar yenkure <sagaryenkure@vionsys.com>
2025-02-25 18:27:19 +0530

    feat: Update AdminLayout buttons for improved accessibility and visual consistency; replace icons and adjust styles

--- src/components/admin/AdminLayout.tsx
+++ src/components/admin/AdminLayout.tsx
@@ -9,7 +9,7 @@ import ProductCard from "../home/ProductCard";
 import { ProductCardSkeleton } from "../Skeletons";
 import { Modal } from "antd";
 import AdminNotification from "./AdminNotification";
-import { PlusCircle } from "lucide-react";
+import { BadgeIndianRupee, PlusCircle } from "lucide-react";
 import { IconNotification } from "@tabler/icons-react";
 import VoucherForm from "./VoucherForm";
 
@@ -30,23 +30,23 @@ const AdminLayout = () => {
       <div className="flex flex-wrap justify-center w-full items-center mx-auto px-2 py-6">
         <button
           onClick={() => setModalOpen(!ModalOpen)}
-          className="w-full sm:w-auto shadow-[0_0_0_3px_#000000_inset] m-2 text-center flex gap-2 items-center px-3 py-2 bg-transparent border border-white text-white rounded-lg font-bold transform hover:-translate-y-1 transition duration-400"
+          className="w-full sm:w-auto shadow-[0_0_0_3px_#000000_inset] m-2 pr-9 text-center justify-center flex gap-2 items-center px-3 py-2 bg-transparent border border-gray-400  text-white rounded-lg font-bold transform hover:-translate-y-1 transition duration-400"
         >
           <PlusCircle />
-          Add Product
+          Products
         </button>
         <button
           onClick={() => setNotificationModalOpen(!NotificationModalOpen)}
-          className="w-full sm:w-auto shadow-[0_0_0_3px_#000000_inset] m-2 flex gap-2 items-center px-2 py-2 bg-transparent border border-white text-white rounded-lg font-bold transform hover:-translate-y-1 transition duration-400"
+          className="w-full sm:w-auto shadow-[0_0_0_3px_#000000_inset] m-2 flex gap-2 justify-center items-center px-2 py-2 bg-transparent border border-gray-400 text-white rounded-lg font-bold transform hover:-translate-y-1 transition duration-400"
         >
           <IconNotification />
-          Notification
+          Notifications
         </button>
         <button
           onClick={() => setVoucherModal(!VoucherModal)}
-          className="w-full sm:w-auto shadow-[0_0_0_3px_#000000_inset] m-2 flex gap-2 items-center px-2 py-2 bg-transparent border border-white text-white rounded-lg font-bold transform hover:-translate-y-1 transition duration-400"
+          className="w-full sm:w-auto shadow-[0_0_0_3px_#000000_inset] m-2 pr-8 flex gap-2 justify-center items-center px-2 py-2 bg-transparent border border-gray-400 text-white rounded-lg font-bold transform hover:-translate-y-1 transition duration-400"
         >
-          <IconNotification />
+          <BadgeIndianRupee />
           Vouchers
         </button>
       </div>
