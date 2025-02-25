6a5eb06ee08b7d3324edfdce52a39b7239c7a7ac
sagar yenkure <sagaryenkure@vionsys.com>
2025-02-25 13:11:39 +0530

    chore: Comment out useAddOrder hook implementation for future reference

--- src/features/orderMutations/useAddOrder.ts
+++ src/features/orderMutations/useAddOrder.ts
@@ -1,22 +1,22 @@
-import { addOrderApi, IAddOrder } from "@/services/orderApi";
-import { useMutation } from "@tanstack/react-query";
-import toast from "react-hot-toast";
+// import { addOrderApi, IAddOrder } from "@/services/orderApi";
+// import { useMutation } from "@tanstack/react-query";
+// import toast from "react-hot-toast";
 
-const useAddOrder = () => {
-  const { mutate, isPending, isError, data } = useMutation({
-    mutationFn: ({ productId, variant }: IAddOrder) =>
-      addOrderApi({ productId, variant }),
+// const useAddOrder = () => {
+//   const { mutate, isPending, isError, data } = useMutation({
+//     mutationFn: ({ productId, variant }: IAddOrder) =>
+//       addOrderApi({ productId, variant }),
 
-    onSuccess: () => toast.success("order created successfully!"),
-    onError: (err: any) => toast.error(err.response.data.error),
-  });
+//     onSuccess: () => toast.success("order created successfully!"),
+//     onError: (err: any) => toast.error(err.response.data.error),
+//   });
 
-  return {
-    orderData: data,
-    addOrder: mutate,
-    addOrderLoading: isPending,
-    addOrderError: isError,
-  };
-};
+//   return {
+//     orderData: data,
+//     addOrder: mutate,
+//     addOrderLoading: isPending,
+//     addOrderError: isError,
+//   };
+// };
 
-export default useAddOrder;
+// export default useAddOrder;
